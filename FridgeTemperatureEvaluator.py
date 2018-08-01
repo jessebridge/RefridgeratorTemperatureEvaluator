8d875d06e30de63760160d690112e99fe9b01798
Jesse <jesse.bridge@my.jcu.edu.au>
2018-08-01 16:37:56 +1000

    Initial commit

---
+++ FridgeTemperatureEvaluator.py
@@ -0,0 +1,13 @@
+def load_json_file(jason_file):
+    json_file_as_list = []
+    print(jason_file)
+    # open json file
+    return json_file_as_list
+
+
+def main():
+     json_file = input("Insert directory of json file here --> ")
+     # json_file =
+     json_file_as_list = load_json_file(json_file)
+
+main()

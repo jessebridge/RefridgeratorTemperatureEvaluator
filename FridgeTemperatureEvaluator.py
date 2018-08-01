77eac1454b510f81bceca441ef879cc33fa2b1da
Jesse <jesse.bridge@my.jcu.edu.au>
2018-08-01 19:16:47 +1000

    Created a function that is able to pull the json format apart and store the ids as keys, then store the temperatures for each key

--- FridgeTemperatureEvaluator.py
+++ FridgeTemperatureEvaluator.py
@@ -1,13 +1,55 @@
-def load_json_file(jason_file):
-    json_file_as_list = []
-    print(jason_file)
-    # open json file
-    return json_file_as_list
+import json
 
+
+def load_json_file():
+    sorted_data = {}
+    json_object_data = [{"id": "a", "timestamp": 1509493641, "temperature": 3.53},
+                        {"id": "b", "timestamp": 1509493642, "temperature": 4.13},
+                        {"id": "c", "timestamp": 1509493643, "temperature": 3.96},
+                        {"id": "a", "timestamp": 1509493644, "temperature": 3.63},
+                        {"id": "c", "timestamp": 1509493645, "temperature": 3.96},
+                        {"id": "a", "timestamp": 1509493645, "temperature": 4.63},
+                        {"id": "a", "timestamp": 1509493646, "temperature": 3.53},
+                        {"id": "b", "timestamp": 1509493647, "temperature": 4.15},
+                        {"id": "c", "timestamp": 1509493655, "temperature": 3.95},
+                        {"id": "a", "timestamp": 1509493677, "temperature": 3.66},
+                        {"id": "b", "timestamp": 1510113646, "temperature": 4.15},
+                        {"id": "c", "timestamp": 1510127886, "temperature": 3.36},
+                        {"id": "c", "timestamp": 1510127892, "temperature": 3.36},
+                        {"id": "a", "timestamp": 1510128112, "temperature": 3.67},
+                        {"id": "b", "timestamp": 1510128115, "temperature": 3.88}]
+    # Loop through each item in the array
+    for element in json_object_data:
+        if element["id"] not in sorted_data.keys():
+            sorted_data[element["id"]] = [element["temperature"]]
+        else:
+            sorted_data[element["id"]].append(element["temperature"])
+    return sorted_data
+
+
+#
+# def calculate_mean():
+#
+#
+# # calculate the mode for each Id
+# def calculate_median():
+#
+#
+# # calculate the median for each Id
+# def calculate_mode():
+#
+#
+# # calculate the mode for each Id
+#
+# def write_json_format():
+#
 
 def main():
-     json_file = input("Insert directory of json file here --> ")
-     # json_file =
-     json_file_as_list = load_json_file(json_file)
+    json_file_as_list = load_json_file()
+    # calculate_mean(json_file_as_list)
+    # calculate_median(json_file_as_list)
+    # calculate_mode(json_file_as_list)
+    # write_json_format()
+
 
 main()

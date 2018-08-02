eb432e276e76263ae8cd5693176e29425106fefb
Jesse <jesse.bridge@my.jcu.edu.au>
2018-08-02 14:31:05 +1000

    most of the mode calculation method has been completed

--- FridgeTemperatureEvaluator.py
+++ FridgeTemperatureEvaluator.py
@@ -1,10 +1,13 @@
+from collections import Counter
+
+
 def convert_data(json_object_data):
     json_file_as_dictionary = load_json_file(json_object_data)
     calculate_average(json_file_as_dictionary)
     calculate_median(json_file_as_dictionary)
     calculate_mode(json_file_as_dictionary)
-    # write_json_format(json_file_as_dictionary)
-    #
+    # final_result = write_json_format(json_file_as_dictionary)
+    # return final_result
 
 
 def load_json_file(json_object_data):
@@ -26,7 +29,7 @@ def calculate_average(json_file_as_dictionary):
         average = sum_of_values / num_of_values
         average = round(average, 2)
         print(key, average)
-        #       need to consider how to return this either as a dictionary or variable
+        # need to consider how to return this either as a dictionary or variable
 
 
 def calculate_median(json_file_as_dictionary):
@@ -44,13 +47,21 @@ def calculate_median(json_file_as_dictionary):
             median_number = (value[array_length // 2] + value[array_length // 2 - 1]) / 2
             median_number = round(median_number, 2)
             print(key, median_number)
-
             # find number on either side of this position -1 and +1 then add the 2 values together and divide
 
 
-def calculate_mode(jason_file_as_list):
-    print("working on")
-
+def calculate_mode(json_file_as_dictionary):
+    for key, value in json_file_as_dictionary.items():
+        # print(value)
+        calc = {}
+        for item in value:
+            if item not in calc.keys():
+                calc[item] = 0
+            else:
+                calc[item] += 1
+        maximum_value = max(calc.values())
+        print(maximum_value)
+        print(calc)
 
 def main():
     json_object_data = [{"id": "a", "timestamp": 1509493641, "temperature": 3.53},
@@ -68,6 +79,8 @@ def main():
                         {"id": "c", "timestamp": 1510127892, "temperature": 3.36},
                         {"id": "a", "timestamp": 1510128112, "temperature": 3.67},
                         {"id": "b", "timestamp": 1510128115, "temperature": 3.88}]
-    convert_data(json_object_data)
+    final_result = convert_data(json_object_data)
+    print(final_result)
+
 
 main()
